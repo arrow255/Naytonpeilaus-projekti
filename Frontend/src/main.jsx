e2be846e986e0f7658d19dcdc2eb22e949490213
Jimi Jukkala <jukkalajimi@gmail.com>
2025-09-30 17:37:31 +0300

    Styling fix

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,24 +1,25 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+// Sites
 import Client from "./sites/client/Client"
 import Host from "./sites/host/Host"
 import CreateRoom from "./sites/createRoom/CreateRoom"
 import JoinRoom from "./sites/joinRoom/JoinRoom"
-
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 import App from "./App.jsx"
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/host' element={<Host />} />
-        <Route path='/client' element={<Client />} />
-        <Route path='/' element={<App />} />
-        <Route path='/create_room' element={<CreateRoom/>}/>
-        <Route path='/join_room' element={<JoinRoom/>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/host' element={<Host />} />
+          <Route path='/client' element={<Client />} />
+          <Route path='/' element={<App />} />
+          <Route path='/create_room' element={<CreateRoom/>}/>
+          <Route path='/join_room' element={<JoinRoom/>}/>
+        </Routes>
+      </BrowserRouter>
   </StrictMode>
 )
