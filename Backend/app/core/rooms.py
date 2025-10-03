d914f47885897bd76464542c19ebe6fd0f9d600f
Samuli Peura <samulipeura00@gmail.com>
2025-10-03 15:10:33 +0300

    more

--- Backend/app/core/rooms.py
+++ Backend/app/core/rooms.py
@@ -12,7 +12,7 @@ def _generate_room_id() -> str:
 class Room:
     def __init__(self, roomid: str, host: WebSocket):
         self.roomid = roomid
-        self.users = {}
+        self.users = set()
         self.host = host
     async def remove_user(self, user: WebSocket):
         if user == self.host:
@@ -21,12 +21,12 @@ class Room:
             return
         if user in self.users:
             logger.debug(f"User removed from room {self.roomid}: {user.user_object.name}")
-            del self.users[user.name]
+            self.users.remove(user)
             if self.host and self.host != user:
                 await self.host.send_json({"type": "USER_LEFT", "username": user.user_object.name})        
 
     async def broadcast(self, message: dict):
-        for user in self.users.values():
+        for user in self.users:
             logger.debug(f"Sending to user: {user.name} | message: {message}")
             await user.send_json(message)
     async def send_host(self, message: dict):
@@ -35,14 +35,14 @@ class Room:
             await self.host.send_json(message)
     
     async def add_user(self, user: WebSocket):
-        self.users[user] = user
+        self.users.add(user)
 
 
 class RoomManager:
     def __init__(self):
         self._rooms = {}
 
-    async def create_room(self, host: WebSocket) -> Room:
+    async def create_room(self, host: WebSocket) -> str:
         user = host.user_object
         if user.room:
             await user.room.remove_user(host)
@@ -65,11 +65,10 @@ class RoomManager:
     async def add_user(self, roomid: str, user: WebSocket) -> bool:
         roomid = roomid.strip().lower()
         room = self.get(roomid)
-        for u in room.users:
-            if u.user_object.name == user.user_object.name:
-                raise RuntimeError("Username already exists in this room")
+        if user in room.users:
+            raise RuntimeError("Username already exists in this room")
         await room.add_user(user)
-        user.room = room
+        user.user_object.room = room
         logger.debug(f"User added to room {roomid}: {user.name}")
         return True
     
@@ -81,8 +80,8 @@ class RoomManager:
         room = self.get(roomid)
         await room.remove_user(user)
         if not room.users and not room.host:
-            del self._rooms[self.roomid]
-            logger.debug(f"Room removed: {self.roomid}")
+            del self._rooms[roomid]
+            logger.debug(f"Room removed: {roomid}")
 
 
 
