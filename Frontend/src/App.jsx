cf86d3aa59537684a07bd62b83c151a71489a66e
arrow255 <santeri3akkanen@gmail.com>
2025-10-07 13:46:41 +0300

    tehty alkusivua valmiimmaksi visuaalisesti, lisätty chakra UIn komponentit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,52 @@
-import "./App.css"
-import Client from "./sites/client/Client"
-import { Routes, Route, Link } from "react-router-dom"
+// src/App.jsx
+import { PinInput, Box, VStack, Heading, Button, ButtonGroup } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 
+//TODO: luo huone näkyviin vasta jos kirjautunut? Jos se on järkevä sillee
 function App() {
   return (
-    <>
-      <h1>Näytönpeilaus projekti</h1>
-      
-      <Link to='/create_room'>
-        <button>Create Room</button>
-      </Link>
+    <Box bg="yellow.100" minH="100vh" w="100%" p={4}>
+      <VStack spacing={3} align="end" mt={1}>
+        <Button 
+        as={Link} to="/join_room" 
+        colorPalette="teal" 
+        size="xl" 
+        variant="surface"
+        >
+          Kirjaudu sisään
+        </Button>
 
-      <Link to='/join_room'>
-        <button>Join Room</button>
-      </Link>
-    </>
+        <Button 
+        as={Link} to="/create_room" 
+        colorPalette="teal" 
+        size="xl" 
+        variant="surface"
+        > 
+          Luo huone   
+        </Button>
+      </VStack>
+        
+      <Box minH="100vh" display="flex" justifyContent="center" alignItems="center">
+        <VStack spacing={7} align="center">
+          <Box width="100%" padding="4" color="black" fontSize="3xl">
+            Liity opettajan huoneeseen syöttämällä liittymiskoodi:
+          </Box>
+          
+          <PinInput.Root type="alphanumeric" placeholder="">
+            <PinInput.HiddenInput />
+            <PinInput.Control>
+              <PinInput.Input index={0} w="80px" h="80px" fontSize="3xl" borderWidth="3px" borderColor="black" />
+              <Box padding="2"></Box>
+              <PinInput.Input index={1} w="80px" h="80px" fontSize="3xl" borderWidth="3px" borderColor="black"/>
+              <Box padding="4" fontSize="5xl" lineHeight="40px">-</Box>
+              <PinInput.Input index={2} w="80px" h="80px" fontSize="3xl" borderWidth="3px" borderColor="black"/>
+              <Box padding="2"></Box>
+              <PinInput.Input index={3} w="80px" h="80px" fontSize="3xl" borderWidth="3px" borderColor="black"/>
+            </PinInput.Control>
+          </PinInput.Root>
+        </VStack>
+      </Box>
+    </Box>
   )
 }
 
