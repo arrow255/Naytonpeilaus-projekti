86af16aaf60b04b0055f7838c353f46636671931
arrow255 <santeri3akkanen@gmail.com>
2025-11-04 13:17:15 +0200

    muokattu demoamista varten, laitettu keskeneräisiä komponentteja kommentteihin

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
 // src/App.jsx
-import { PinInput, Box, VStack, Button } from "@chakra-ui/react"
+import { PinInput, Box, VStack, Button, Text } from "@chakra-ui/react"
 import { Link } from "react-router-dom"
 
 //TODO: luo huone näkyviin vasta jos kirjautunut? Jos se on järkevä sillee
 function App() {
   return (
-    <Box bg="yellow.100" minH="100vh" w="100%" p={4}>
-      {/* kirjautumis ja huoneen luomis napit */}
+    <Box bg="yellow.100" minH="100vh" w="100%">
+      {/* kirjautumis ja huoneen luomis napit 
       <VStack spacing={3} align="end" mt={1}>
         <Button 
         as={Link} to="/join_room" 
@@ -17,14 +17,24 @@ function App() {
           Kirjaudu sisään
         </Button>
       </VStack>
+      */}
+       <Box p={4} textAlign="center" bgGradient="to-b" gradientFrom="orange.200" gradientTo="yellow.100">
+        <Text fontSize="5xl" fontWeight="bold">
+          Jaa näyttösi helposti!
+        </Text>
+      </Box>
         
+
       <Box display="flex" justifyContent="center" alignItems="center" minHeight="48vh">
+        
+        
         <VStack spacing={7} align="center">
+          {/* pin koodi paikka
           <Box width="100%" padding="4" color="black" fontSize="3xl">
             Liity opettajan huoneeseen syöttämällä liittymiskoodi:
           </Box>
           
-          {/* pin koodi paikka */}
+           
           <PinInput.Root type="alphanumeric" placeholder="" >
             <PinInput.HiddenInput />
             <PinInput.Control>
@@ -37,11 +47,11 @@ function App() {
               <PinInput.Input index={3} w="80px" h="80px" fontSize="3xl" borderWidth="3px" borderColor="black"/>
             </PinInput.Control>
           </PinInput.Root>
-
+          
           <Box padding="4" color="black" fontSize="3xl" justifyContent="center" alignItems="center">
             TAI
           </Box>
-
+          */}
           <Button 
           as={Link} to="/create_room" 
           colorPalette="teal" 
@@ -52,7 +62,17 @@ function App() {
           > 
             Luo huone   
           </Button>
-
+          
+          <Button 
+          as={Link} to="/join_room" 
+          colorPalette="teal" 
+          fontSize = {30}
+          px={10}
+          py={10} 
+          variant="surface"
+          >
+            Kirjaudu sisään
+          </Button>
         </VStack>
       </Box>
     </Box>
